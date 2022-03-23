edf3a14413ba226ae66e9fccfbfbc4d672294c12
jakob spile <jspi@itu.dk>
2022-03-23 11:38:38 +0100

    test

--- app/src/main/java/bfst22/vector/App.java
+++ app/src/main/java/bfst22/vector/App.java
@@ -7,7 +7,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // var filename = getParameters().getRaw().get(0);
-        var model = new Model("data/denmark.zip");
+        var model = new Model("data/Bornholm.zip");
         new View(model, primaryStage);
     }
 }
