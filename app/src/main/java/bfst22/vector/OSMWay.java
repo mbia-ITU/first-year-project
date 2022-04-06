ec46f2a7b523ed5a4d1d33b34c298d1dac194434
jakob spile <jspi@itu.dk>
2022-04-06 13:45:59 +0200

    kdTree

--- app/src/main/java/bfst22/vector/OSMWay.java
+++ app/src/main/java/bfst22/vector/OSMWay.java
@@ -1,8 +1,7 @@
 package bfst22.vector;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import javax.management.RuntimeErrorException;
 
@@ -12,6 +11,9 @@ public class OSMWay implements Serializable {
     public static final long serialVersionUID = 42;
     List<OSMNode> nodes;
     float minX = Float.MAX_VALUE, minY = Float.MAX_VALUE, maxX = Float.MIN_VALUE, maxY = Float.MIN_VALUE;
+    Comparator<OSMWay> OSMWayComparatorX = Comparator.comparing(list -> list.getPlotBounds().getCenterX());
+    Comparator<OSMWay> OSMWayComparatorY = Comparator.comparing(list -> list.getPlotBounds().getCenterY());
+    List<OSMWay> listOfOSMWays;
     
     public OSMWay(List<OSMNode> nodes) {
         this.nodes = new ArrayList<>(nodes);
@@ -36,4 +38,47 @@ public class OSMWay implements Serializable {
         return new BoundingBox(minX, maxX, minY, maxY);
     }
 
+    public List<List<OSMWay>> splitOnX(List<OSMWay> list){
+        List<OSMWay> leftList;
+        List<OSMWay> rightList;
+
+        if (list.size() < 1000) {
+            this.listOfOSMWays = list;
+            List<List<OSMWay>> listOfOSMWays = new ArrayList<List<OSMWay>>();
+            listOfOSMWays.add(this.listOfOSMWays);
+            return listOfOSMWays;
+        } else {
+            Collections.sort(list, OSMWayComparatorX);
+            leftList = list.subList(0, list.size() / 2);
+            rightList = list.subList(list.size() / 2, list.size());
+            List<List<OSMWay>> listOfOSMWays = new ArrayList<List<OSMWay>>();
+            listOfOSMWays.add(leftList);
+            listOfOSMWays.add(rightList);
+
+            return listOfOSMWays; 
+        }
+    
+    }
+
+    public List<List<OSMWay>> splitOnY(List<OSMWay> list){
+        List<OSMWay> leftList;
+        List<OSMWay> rightList;
+
+        if (list.size() < 1000) {
+            this.listOfOSMWays = list;
+            List<List<OSMWay>> listOfOSMWays = new ArrayList<List<OSMWay>>();
+            listOfOSMWays.add(this.listOfOSMWays);
+            return listOfOSMWays;
+        } else {
+            Collections.sort(list, OSMWayComparatorY);
+            leftList = list.subList(0, list.size() / 2);
+            rightList = list.subList(list.size() / 2, list.size());
+            List<List<OSMWay>> listOfOSMWays = new ArrayList<List<OSMWay>>();
+            listOfOSMWays.add(leftList);
+            listOfOSMWays.add(rightList);
+
+            return listOfOSMWays; 
+        }
+    
+    }
 }
