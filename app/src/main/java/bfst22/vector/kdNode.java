bd23cc6b8b9715e8038481d68cc7f50c8c4cc6e1
Mikkel Bistrup Andersen <mbia@itu.dk>
2022-04-20 10:27:07 +0200

    prepared for new kdnode

--- app/src/main/java/bfst22/vector/kdNode.java
+++ app/src/main/java/bfst22/vector/kdNode.java
@@ -10,52 +10,5 @@ public class kdNode {
     public kdNode(List<OSMWay> list) {
         box = box.combineMany(list);
     }
-<<<<<<< HEAD
-
-    public List<List<OSMWay>> splitOnX(List<OSMWay> list) {
-        List<OSMWay> leftList;
-        List<OSMWay> rightList;
-
-        if (list.size() < 500) {
-            this.listOfOSMWays = list;
-            List<List<OSMWay>> listOfOSMWays = new ArrayList<List<OSMWay>>();
-            listOfOSMWays.add(this.listOfOSMWays);
-            return listOfOSMWays;
-        } else {
-            Collections.sort(list, OSMWayComparatorX);
-            leftList = list.subList(0, list.size() / 2);
-            rightList = list.subList(list.size() / 2, list.size());
-            List<List<OSMWay>> listOfOSMWays = new ArrayList<List<OSMWay>>();
-            listOfOSMWays.add(leftList);
-            listOfOSMWays.add(rightList);
-
-            return listOfOSMWays;
-        }
-
-    }
-
-    public List<List<OSMWay>> splitOnY(List<OSMWay> list) {
-        List<OSMWay> leftList;
-        List<OSMWay> rightList;
-
-        if (list.size() < 500) {
-            this.listOfOSMWays = list;
-            List<List<OSMWay>> listOfOSMWays = new ArrayList<List<OSMWay>>();
-            listOfOSMWays.add(this.listOfOSMWays);
-            return listOfOSMWays;
-        } else {
-            Collections.sort(list, OSMWayComparatorY);
-            leftList = list.subList(0, list.size() / 2);
-            rightList = list.subList(list.size() / 2, list.size());
-            List<List<OSMWay>> listOfOSMWays = new ArrayList<List<OSMWay>>();
-            listOfOSMWays.add(leftList);
-            listOfOSMWays.add(rightList);
-
-            return listOfOSMWays;
-        }
-
-    }
-=======
->>>>>>> 8955565162f8c30bba6af4a06a1f775a91f5365e
 }
  
