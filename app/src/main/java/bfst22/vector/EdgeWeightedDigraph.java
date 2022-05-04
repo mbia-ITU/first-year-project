174aafab9229ab1f5b8fd3e477aefa4bd4771db6
Nicolaj <nihp@itu.dk>
2022-05-04 20:04:27 +0200

    same as previous?

--- app/src/main/java/bfst22/vector/EdgeWeightedDigraph.java
+++ app/src/main/java/bfst22/vector/EdgeWeightedDigraph.java
@@ -3,7 +3,15 @@ package bfst22.vector;
 import java.util.*;
 
 public class EdgeWeightedDigraph {
-    public Map<OSMNode, List<DirectedEdge>> adj =;
-    public int wat; 
+    public Map<OSMNode, List<DirectedEdge>> adj;
+    private int[] indegree;
 
+    public EdgeWeightedDigraph() {
+        
+    }
+
+    public int V() {
+        return adj.size();
+    }
 }
+
