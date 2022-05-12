b21ab10a9e4e8388ba1ebfa9891cd56168237801
Nicol <nihp>
2022-05-12 21:10:55 +0200

    testing
    added testing classes

---
+++ app/src/test/java/bfst22/vector/TestProg.java
@@ -0,0 +1,15 @@
+package bfst22.vector;
+
+import java.lang.Object;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestProg {
+    
+    @Test
+    void testAll(){
+        assertEquals(1, 1);
+        
+    }
+
+}
