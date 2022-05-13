275e4713c9ea7540569cc981adfcdcbe4ac576d0
Nicol <nihp>
2022-05-13 12:31:10 +0200

    javadoc
    javadoc for app and main

--- app/src/main/java/bfst22/vector/App.java
+++ app/src/main/java/bfst22/vector/App.java
@@ -3,12 +3,15 @@ package bfst22.vector;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * main application class called in the main method {@code Main}
+ */
 public class App extends Application {
+    /**
+     * the start method called in main launching a new {@code View} class
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // var filename = getParameters().getRaw().get(0);
-        // var model = new Model("data/Bornholm.zip");
         new View();
-        // new View(model, primaryStage);
     }
 }
--- app/src/main/java/bfst22/vector/Main.java
+++ app/src/main/java/bfst22/vector/Main.java
@@ -2,7 +2,14 @@ package bfst22.vector;
 
 import javafx.application.Application;
 
+/**
+ * the main method, launcing the program through the app class using javaFX Application
+ */
 public class Main {
+    /**
+     * main method
+     * @param args the arguments of the main method
+     */
     public static void main(String[] args) {
         Application.launch(App.class, args);
     }
